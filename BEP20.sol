4e6561e8980603b02f6d4c2c005305bf7df9fb6b
MoonWalker <7510709+MoonWalkerJim@users.noreply.github.com>
2021-07-15 00:14:31 +0100

    Delete BEP20.sol

--- BEP20.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.5.17;
-
-import '../MoonWalkerSwapBEP20.sol';
-
-contract BEP20 is MoonWalkerSwapBEP20 {
-    constructor(uint _totalSupply) public {
-        _mint(msg.sender, _totalSupply);
-    }
-}
